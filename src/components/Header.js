f53421e8f9a71ad348833674847ba8c2d6eb162e
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2021-04-10 12:43:41 -0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,8 @@ import Tooltip from 'react-bootstrap/lib/Tooltip';
 import ExportDatabase from '../containers/ExportDatabase';
 import ImportDatabase from './ImportDatabase';
 
+import logo from '../assets/logo.png';
+
 const tableTooltip = (
     <Tooltip id='table-tooltip'><strong>Create New Table</strong></Tooltip>
 );
@@ -15,9 +17,6 @@ const trashTooltip = (
     <Tooltip id='trash-tooltip'><strong>Clear Current Schema</strong></Tooltip>
 );
 
-const forkTooltip = (
-    <Tooltip id='fork-tooltip'><strong>Fork me on Github</strong></Tooltip>
-);
 
 type Props = {
     dbName: string,
@@ -59,7 +58,12 @@ class Header extends Component<Props> {
                 <div className='container'>
                     <div className='row'>
                         <div className='title col-xs-5 col-sm-4 text-left'>
-                            <strong>Schema Builder</strong>
+                            <img
+                                src={ logo }
+                                alt='logo'
+                                height={ 40 }
+                                style={ { paddingTop: 5 } }
+                            />
                         </div>
                         <div className='db-name col-xs-5 col-sm-4 text-center'>
                             <span><i className='fa fa-database'></i> { dbName }</span>
@@ -108,18 +112,6 @@ class Header extends Component<Props> {
 
                                 <ImportDatabase />
 
-                                <li>
-                                    <OverlayTrigger
-                                        placement='bottom'
-                                        overlay={ forkTooltip }
-                                        delayShow={ 300 }
-                                        rootClose
-                                    >
-                                        <a href='https://github.com/Agontuk/schema-designer'>
-                                            <i className='fa fa-github fa-lg' />
-                                        </a>
-                                    </OverlayTrigger>
-                                </li>
                             </ul>
                         </div>
                     </div>
